7b17ecb464e2adbaa4e8ec360b4a210b4a602084
Jatson <back.barna@gmail.com>
2021-01-24 19:04:33 +0100

    step per mm calibration

--- firmware/V2.0/Marlin-2.0.7.2-SKR-mini-E3-V2.0/Marlin/Configuration.h
+++ firmware/V2.0/Marlin-2.0.7.2-SKR-mini-E3-V2.0/Marlin/Configuration.h
@@ -744,7 +744,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 78.82, 80, 390.24, 416.33 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 78.82, 81.184, 390.24, 416.33 }
 
 /**
  * Default Max Feed Rate (mm/s)
